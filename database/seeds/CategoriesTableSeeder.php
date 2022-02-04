154d1c032f3c10fe280e4016ffbf5968481d61b4
francescopalmisanodev <francescopalmisano.dev@gmail.com>
2022-02-04 17:06:22 +0100

    update migartion

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Category;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['HTML', 'CSS', 'PHP', 'JavaScript'];
+
+        foreach ($categories as $category) {
+            $newCategory = new Category();
+            $newCategory->title = $category['title'];
+            $newCategory->save();
+        }
+    }
+}
